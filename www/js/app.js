cf20fce9206d6ec7e39bc7f00aa2dbce7543cbe4
John D. Storey <johndangerstorey@gmail.com>
2016-03-26 02:54:56 -0600

    successfully deploys first version of app to ionic

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', [
-  'ionic',
+  'ionic','ionic.service.core',
   'starter.menu',
   'starter.myStories',
   'starter.story',
