503338a972679fffb6c7eeee0a37af450678c6c9
John D. Storey <johndangerstorey@gmail.com>
2016-04-03 13:51:35 -0600

    gets style of placeholders, and binding of variables working

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./www/**/**/*.scss']
+  sass: ['./www/templates/**/*.scss','./scss/**/*.scss']
 };
 
 gulp.task('default', ['sass']);
